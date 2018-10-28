81f075d962d86b8079ef8f01245541b0cbca1adc
Duncan Grubbs <duncan.grubbs@gmail.com>
2018-10-28 12:08:54 -0400

    Update to HTTPS url.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     gon.last_query = cookies[:last_query]
     gon.host_url = Rails.env.development? ?
       'http://localhost:3000' :
-      'http://skedgev2.herokuapp.com'
+      'https://skedgev2.herokuapp.com'
   end
 
 end
