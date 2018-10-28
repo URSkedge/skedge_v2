48fa04b03643b345510d210e311c102979aac015
zino <rhu5@u.rochester.edu>
2018-10-27 21:49:36 -0400

    Fix workshop label

--- app/javascript/components/Calendar.jsx
+++ app/javascript/components/Calendar.jsx
@@ -51,16 +51,32 @@ class Calendar extends Component {
     $("#calendar").fullCalendar('removeEvents');
     let sections = this.data[this.data.term];
     let totalCredit = 0;
-    sections.forEach(section => { totalCredit += parseInt(section.course.credits) });
+    sections.forEach(section => {
+      if (section.sectionType == 0) {
+        totalCredit += parseInt(section.course.credits);
+      }
+    });
+
     this.setState({ sectionCnt: sections.length, credits: totalCredit });
     console.log(sections.length);
 
     sections.forEach(section => {
       let start = section.startTime.toString().padStart(4, '0');
       let end = section.endTime.toString().padStart(4, '0');
+      let title = `${section.course.dept} ${section.course.num} `;
+      if (section.sectionType === 1) {
+        title += "(Lab)";
+      } else if (section.sectionType === 2) {
+        title += "(Rec.)";
+      } else if (section.sectionType === 3) {
+        title += "(Lab Lec.)";
+      } else if (section.sectionType === 4) {
+        title += "(Wksp.)";
+      }
+
       $("#calendar").fullCalendar('renderEvent', {
         id: section.crn,
-        title: `${section.course.dept} ${section.course.num}`,
+        title: title,
         start: `${start.substring(0,2)}:${start.substring(2)}:00`,
         end: `${end.substring(0,2)}:${end.substring(2)}:00`,
         resourceIds: section.days.split(''),
